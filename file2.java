e31bc521ac934c4b36d332c9e3747c77c99afc34
Bhumika119 <co.2023.bakamble@bitwardha.ac.in>
2024-09-17 15:07:20 +0530

    Add files via upload

---
+++ file2.java
@@ -0,0 +1,15 @@
+import java.util.*;
+public class File2
+{
+	public static void main(string args[])
+{
+Scanner s1=new Scanner(System.in);
+System.out.println("Enter the 2 number");
+int a=s1.nextInt();
+int b=s1.nextInt();
+int x=a+b;
+System.out.println("Sum"+x);
+
+
+}
+}
